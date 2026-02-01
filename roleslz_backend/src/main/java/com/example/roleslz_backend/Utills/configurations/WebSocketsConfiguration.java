10950b14e1c715fb6621c1ead76f85e0f7b450a3
Iury Benicio <iury620@gmail.com>
2026-01-31 22:40:12 -0300

    Feat: Implementando HeartBeat (proteger contra excesso de requisições)

--- roleslz_backend/src/main/java/com/example/roleslz_backend/Utills/configurations/WebSocketsConfiguration.java
+++ roleslz_backend/src/main/java/com/example/roleslz_backend/Utills/configurations/WebSocketsConfiguration.java
@@ -2,6 +2,7 @@ package com.example.roleslz_backend.Utills.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,8 +13,18 @@ public class WebSocketsConfiguration implements WebSocketMessageBrokerConfigurer
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config){
+        // Criamos um agendador de tarefas para os batimentos
+        ThreadPoolTaskScheduler te = new ThreadPoolTaskScheduler();
+        te.setPoolSize(1);
+        te.setThreadNamePrefix("ws-heartbeat-");
+        te.initialize();
+
         // Habilita um broker simples na memória para enviar mensagens aos clientes
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/topic")
+                // Primeiro valor: quanto o servidor envia (10s)
+                // Segundo valor: quanto o servidor espera receber (10s)
+                .setHeartbeatValue(new long[]{10000, 10000})
+                .setTaskScheduler(te);
         // Prefixo para mensagens que saem do cliente para o servidor
         config.setApplicationDestinationPrefixes("/app");
     }
